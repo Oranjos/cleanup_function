2837de58b691094b7265ad073f01a2a5dc560d6c
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2023-10-06 12:35:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import CleanupFunction from "./Cleanup";
+
+function App() {
+  return (
+    <>
+      <CleanupFunction />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Cleanup.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const CleanupFunction = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="bot">
+      <button
+        className="btn"
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log("hmm, this is interesting");
+    const intId = setInterval(() => {
+      console.log("hello from interval");
+    }, 1000);
+    return () => {
+      clearInterval(intId);
+    };
+  }, []);
+  return <h1 className="biz">hello there</h1>;
+};
+
+export default CleanupFunction;
